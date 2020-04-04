e4dcd38563d6ecbb6a7f186273cde8d0c6a2448d
Julia Debecka <57728399+jdebecka@users.noreply.github.com>
2020-04-04 14:30:50 +0200

    initial commit

---
+++ Joke Bank App/Joke Bank/Joke Bank/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Joke Bank
+//
+//  Created by Julia Debecka on 04/04/2020.
+//  Copyright © 2020 Julia Debecka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
